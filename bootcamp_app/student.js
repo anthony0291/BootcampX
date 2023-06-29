fd2ab6c7cb86391041c7a3757c850e10418e7d96
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-29 03:25:13 +0000

    parameterized query

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -8,15 +8,30 @@ const pool = new Pool({
 });
 
 
-// pool.query is a function that accepts an SQL query as a JavaScript string.
+
+
+//QUERY PARAMETERS adding node.js FEB2 feature
+// // pool.query is a function that accepts an SQL query as a JavaScript string.
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+//   .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     });
+//   }).catch(err => console.error('query error', err.stack));
 
 
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE $1
+LIMIT $2
 `)
   .then(res => {
     res.rows.forEach(user => {
@@ -24,4 +39,3 @@ LIMIT ${process.argv[3] || 5};
     });
   }).catch(err => console.error('query error', err.stack));
 
-//QUERY PARAMETERS adding node.js FEB2 feature
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,13 +8,29 @@ const pool = new Pool({
 });
 
 
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+//   .then(res => {
+//     res.rows.forEach(row => {
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     });
+//   });
+
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = '$1] || 'JUL02'}'
 ORDER BY teacher;
 `)
   .then(res => {
@@ -22,4 +38,3 @@ ORDER BY teacher;
       console.log(`${row.cohort}: ${row.teacher}`);
     });
   });
-
